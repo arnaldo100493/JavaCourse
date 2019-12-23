14e400a0fc4a4fe71901065536964e6140348aa3
Arnaldo Andrés Barrios Mena <proveedor_shernandez@ath.com.co>
2019-12-23 14:42:58 -0500

    Objetos Mutables e Inmutables Java.

---
+++ JavaCourse/src/stringbuilder/StringBuilderExample.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringbuilder;
+
+/**
+ *
+ * @author abarrime
+ */
+public class StringBuilderExample {
+   
+   public StringBuilderExample(){
+       
+   }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s1 = "Java"; 
+        String s2 = "Java";
+        
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append("Ja").append("va");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(sb1.toString() == s1);
+        System.out.println(sb1.toString().equals(s1));
+    }
+    
+}
